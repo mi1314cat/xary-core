59fc43ca991659cb175fc4658b90952b64ac86ab
mi1314cat <135564322+mi1314cat@users.noreply.github.com>
2024-12-06 10:46:12 +0800

    Update vless.sh

--- vless.sh
+++ vless.sh
@@ -190,12 +190,12 @@ http {
             proxy_redirect off;
             proxy_ssl_server_name on;
             sub_filter_once off;
-            sub_filter "pan.imcxx.com" $server_name;
+            sub_filter "pan.imcxx.com" \$server_name;
             proxy_set_header Host "pan.imcxx.com";
-            proxy_set_header Referer $http_referer;
-            proxy_set_header X-Real-IP $remote_addr;
-            proxy_set_header User-Agent $http_user_agent;
-            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+            proxy_set_header Referer \$http_referer;
+            proxy_set_header X-Real-IP \$remote_addr;
+            proxy_set_header User-Agent \$http_user_agent;
+            proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
             proxy_set_header X-Forwarded-Proto https;
             proxy_set_header Accept-Encoding "";
             proxy_set_header Accept-Language "zh-CN";
