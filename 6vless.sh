28857aafa449b4bbae5c285d285e218357645494
mi1314cat <135564322+mi1314cat@users.noreply.github.com>
2024-12-06 11:40:08 +0800

    Create 6vless.sh

---
+++ 6vless.sh
@@ -0,0 +1,338 @@
+#!/bin/bash
+
+# 颜色变量定义
+RED="\033[31m"
+GREEN="\033[32m"
+YELLOW="\033[33m"
+PLAIN="\033[0m"
+
+# 检查是否为root用户
+[[ $EUID -ne 0 ]] && echo -e "${RED}错误：${PLAIN} 必须使用root用户运行此脚本！\n" && exit 1
+
+# 系统信息
+SYSTEM_NAME=$(grep -i pretty_name /etc/os-release | cut -d \" -f2)
+CORE_ARCH=$(arch)
+
+# 介绍信息
+clear
+cat << "EOF"
+                       |\__/,|   (\\
+                     _.|o o  |_   ) )
+       -------------(((---(((-------------------
+                   catmi.vless.ws
+       -----------------------------------------
+EOF
+echo -e "${GREEN}System: ${PLAIN}${SYSTEM_NAME}"
+echo -e "${GREEN}Architecture: ${PLAIN}${CORE_ARCH}"
+echo -e "${GREEN}Version: ${PLAIN}1.0.0"
+echo -e "----------------------------------------"
+
+# 打印带颜色的消息
+print_info() {
+    echo -e "${GREEN}[Info]${PLAIN} $1"
+}
+
+print_error() {
+    echo -e "${RED}[Error]${PLAIN} $1"
+}
+
+# 随机生成 UUID
+generate_uuid() {
+    cat /proc/sys/kernel/random/uuid
+}
+
+# 随机生成 WS 路径
+generate_ws_path() {
+    echo "/$(tr -dc 'a-zA-Z0-9' < /dev/urandom | head -c 10)"
+}
+ssl() {
+    set -e
+
+# 提供操作选项供用户选择
+echo "请选择要执行的操作："
+echo "1) 有80和443端口"
+echo "2) 无80 443端口"
+read -p "请输入选项 (1 或 2): " choice
+
+# 提示用户输入域名和电子邮件地址
+read -p "请输入域名: " DOMAIN
+
+# 将用户输入的域名转换为小写
+DOMAIN_LOWER=$(echo "$DOMAIN" | tr '[:upper:]' '[:lower:]')
+
+read -p "请输入电子邮件地址: " EMAIL
+
+# 创建目标目录
+TARGET_DIR="/root/catmi"
+mkdir -p "$TARGET_DIR"
+
+if [ "$choice" -eq 1 ]; then
+    # 选项 1: 安装更新、克隆仓库并执行脚本
+    echo "执行安装acme证书..."
+
+    # 更新系统并安装必要的依赖项
+    echo "更新系统并安装依赖项..."
+    sudo apt update && sudo apt upgrade -y
+    sudo apt install ufw -y
+    sudo apt install -y curl socat git cron openssl
+    ufw disable
+    # 安装 acme.sh
+    echo "安装 acme.sh..."
+    curl https://get.acme.sh | sh
+
+    # 设置路径
+    export PATH="$HOME/.acme.sh:$PATH"
+
+    # 注册账户
+    echo "注册账户..."
+    "$HOME/.acme.sh/acme.sh" --register-account -m "$EMAIL"
+
+    # 申请 SSL 证书
+    echo "申请 SSL 证书..."
+    if ! "$HOME/.acme.sh/acme.sh" --issue --standalone -d "$DOMAIN_LOWER"; then
+        echo "证书申请失败，删除已生成的文件和文件夹。"
+        rm -f "$HOME/${DOMAIN_LOWER}.key" "$HOME/${DOMAIN_LOWER}.crt"
+        "$HOME/.acme.sh/acme.sh" --remove -d "$DOMAIN_LOWER"
+        exit 1
+    fi
+
+    # 安装 SSL 证书并移动到目标目录
+    echo "安装 SSL 证书..."
+    "$HOME/.acme.sh/acme.sh" --installcert -d "$DOMAIN_LOWER" \
+        --key-file       "$TARGET_DIR/${DOMAIN_LOWER}.key" \
+        --fullchain-file "$TARGET_DIR/${DOMAIN_LOWER}.crt"
+        CERT_PATH="$TARGET_DIR/${DOMAIN_LOWER}.crt"
+        KEY_PATH="$TARGET_DIR/${DOMAIN_LOWER}.key"
+
+    # 提示用户证书已生成
+    echo "SSL 证书和私钥已生成并移动到 $TARGET_DIR:"
+    echo "证书: $TARGET_DIR/${DOMAIN_LOWER}.crt"
+    echo "私钥: $TARGET_DIR/${DOMAIN_LOWER}.key"
+
+    # 创建自动续期的脚本
+    cat << EOF > /root/renew_cert.sh
+#!/bin/bash
+export PATH="\$HOME/.acme.sh:\$PATH"
+\$HOME/.acme.sh/acme.sh --renew -d "$DOMAIN_LOWER" --key-file "$TARGET_DIR/${DOMAIN_LOWER}.key" --fullchain-file "$TARGET_DIR/${DOMAIN_LOWER}.crt"
+EOF
+    chmod +x /root/renew_cert.sh
+
+    # 创建自动续期的 cron 任务，每天午夜执行一次
+    (crontab -l 2>/dev/null; echo "0 0 * * * /root/renew_cert.sh >> /var/log/renew_cert.log 2>&1") | crontab -
+
+    echo "完成！请确保在您的 Web 服务器配置中使用新的 SSL 证书。"
+
+elif [ "$choice" -eq 2 ]; then
+    # 选项 2: 手动获取 SSL 证书证书安装/etc/letsencrypt/live/$DOMAIN_LOWER 目录 文件夹
+    echo "将进行手动获取 SSL 证书证书安装/etc/letsencrypt/live/$DOMAIN_LOWER 目录文件夹..."
+    CERT_PATH="/etc/letsencrypt/live/$DOMAIN_LOWER/fullchain.pem"
+    KEY_PATH="/etc/letsencrypt/live/$DOMAIN_LOWER/privkey.pem"
+
+    # 安装 Certbot
+    echo "安装 Certbot..."
+    sudo apt-get update
+    sudo apt-get install -y certbot openssl
+
+    # 手动获取证书
+    echo "手动获取证书..."
+    sudo certbot certonly --manual --preferred-challenges dns -d "$DOMAIN_LOWER"
+
+    
+
+    # 创建自动续期的 cron 任务
+    (crontab -l 2>/dev/null; echo "0 0 * * * certbot renew") | crontab -
+
+
+    echo "SSL 证书已安装/etc/letsencrypt/live/$DOMAIN_LOWER 目录中"
+else
+    echo "无效选项，请输入 1 或 2."
+fi
+}
+
+nginx() {
+    apt install -y nginx
+    cat << EOF > /etc/nginx/nginx.conf
+user www-data;
+worker_processes auto;
+pid /run/nginx.pid;
+include /etc/nginx/modules-enabled/*.conf;
+
+events {
+    worker_connections 1024;
+}
+
+http {
+    sendfile on;
+    tcp_nopush on;
+    tcp_nodelay on;
+    keepalive_timeout 65;
+    types_hash_max_size 2048;
+
+    include /etc/nginx/mime.types;
+    default_type application/octet-stream;
+    gzip on;
+
+    server {
+        listen [::]:${PORT} ssl;
+        server_name ${DOMAIN_LOWER};
+
+        ssl_certificate       "${CERT_PATH}";
+        ssl_certificate_key   "${KEY_PATH}";
+        
+        ssl_session_timeout 1d;
+        ssl_session_cache shared:MozSSL:10m;
+        ssl_session_tickets off;
+        ssl_protocols    TLSv1.2 TLSv1.3;
+        ssl_prefer_server_ciphers off;
+
+        location / {
+            proxy_pass https://pan.imcxx.com; #伪装网址
+            proxy_redirect off;
+            proxy_ssl_server_name on;
+            sub_filter_once off;
+            sub_filter "pan.imcxx.com" \$server_name;
+            proxy_set_header Host "pan.imcxx.com";
+            proxy_set_header Referer \$http_referer;
+            proxy_set_header X-Real-IP \$remote_addr;
+            proxy_set_header User-Agent \$http_user_agent;
+            proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+            proxy_set_header X-Forwarded-Proto https;
+            proxy_set_header Accept-Encoding "";
+            proxy_set_header Accept-Language "zh-CN";
+        }
+
+        location ${WS_PATH} {
+            proxy_redirect off;
+            proxy_pass http://127.0.0.1:9999;
+            proxy_http_version 1.1;
+            proxy_set_header Upgrade \$http_upgrade;
+            proxy_set_header Connection "upgrade";
+            proxy_set_header Host \$host;
+        }
+    }
+}
+EOF
+
+
+    systemctl reload nginx
+}
+# 安装xray
+echo "安装最新 Xray..."
+bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install || { echo "Xray 安装失败"; exit 1; }
+
+mv /usr/local/bin/xray /usr/local/bin/xrayls || { echo "移动文件失败"; exit 1; }
+chmod +x /usr/local/bin/xrayls || { echo "修改权限失败"; exit 1; }
+
+cat <<EOF >/etc/systemd/system/xrayls.service
+[Unit]
+Description=xrayls Service
+After=network.target
+
+[Service]
+ExecStart=/usr/local/bin/xrayls -c /etc/xrayls/config.json
+Restart=on-failure
+RestartSec=3
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+
+# 提示输入监听端口号
+read -p "请输入 Vless 监听端口 (默认为 443): " PORT
+PORT=${PORT:-443}
+# 生成 UUID 和 WS 路径
+UUID=$(generate_uuid)
+WS_PATH=$(generate_ws_path)
+
+
+
+# 获取公网 IP 地址
+PUBLIC_IP_V4=$(curl -s https://api.ipify.org)
+PUBLIC_IP_V6=$(curl -s https://api64.ipify.org)
+echo "公网 IPv4 地址: $PUBLIC_IP_V4"
+echo "公网 IPv6 地址: $PUBLIC_IP_V6"
+
+# 选择使用哪个公网 IP 地址
+echo "请选择要使用的公网 IP 地址:"
+echo "1. $PUBLIC_IP_V4"
+echo "2. $PUBLIC_IP_V6"
+read -p "请输入对应的数字选择: " IP_CHOICE
+
+if [ "$IP_CHOICE" -eq 1 ]; then
+    PUBLIC_IP=$PUBLIC_IP_V4
+elif [ "$IP_CHOICE" -eq 2 ]; then
+    PUBLIC_IP=$PUBLIC_IP_V6
+else
+    echo "无效选择，退出脚本"
+    exit 1
+fi
+ssl
+# 配置文件生成
+mkdir -p /etc/xrayls
+cat <<EOF > /etc/xrayls/config.json
+{
+    "log": {
+        "disabled": false,
+        "level": "info",
+        "timestamp": true
+    },
+    "inbounds": [
+        {
+            "listen": "127.0.0.1",
+            "port": 9999,
+            "tag": "VLESS-WS",
+            "protocol": "VLESS",
+            "settings": {
+                "clients": [
+                    {
+                        "id": "${UUID}",
+                        "alterId": 64
+                    }
+                ],
+                "decryption": "none"
+            },
+            "streamSettings": {
+                "network": "ws",
+                "wsSettings": {
+                    "path": "${WS_PATH}"
+                }
+            }
+        }
+    ],
+    "outbounds": [
+        {
+            "protocol": "freedom",
+            "settings": {}
+        }
+    ]
+}
+EOF
+
+# 重载systemd服务配置
+sudo systemctl daemon-reload
+sudo systemctl enable xrayls
+sudo systemctl restart xrayls || { echo "重启 xrayls 服务失败"; exit 1; }
+
+
+# 保存信息到文件
+OUTPUT_DIR="/root/xrayls"
+mkdir -p "$OUTPUT_DIR"
+{
+    echo "xray 安装完成！"
+    echo "服务器地址：${PUBLIC_IP}"
+    echo "端口：${PORT}"
+    echo "UUID：${UUID}"
+    echo "WS 路径：${WS_PATH}"
+    echo "配置文件已保存到：/etc/xrayls/config.json"
+} > "$OUTPUT_DIR/install_info.txt"
+
+print_info "xray 安装完成！"
+print_info "服务器地址：${PUBLIC_IP}"
+print_info "端口：${PORT}"
+print_info "UUID：${UUID}"
+print_info "WS 路径：${WS_PATH}"
+print_info "配置文件已保存到：/etc/xrayls/config.json"
+
+sudo systemctl status xrayls
+nginx 
