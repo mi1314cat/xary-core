36063e0168f2cbe6d79fac8442bd1280102ed639
mi1314cat <135564322+mi1314cat@users.noreply.github.com>
2025-01-22 00:29:13 +0800

    Update xargo.sh

--- xargo.sh
+++ xargo.sh
@@ -17,7 +17,31 @@ yellow(){
     echo -e "\033[33m\033[01m$1\033[0m"
 }
 
+REGEX=("debian" "ubuntu" "centos|red hat|kernel|oracle linux|alma|rocky" "'amazon linux'" "alpine")
+RELEASE=("Debian" "Ubuntu" "CentOS" "CentOS" "Alpine")
+PACKAGE_UPDATE=("apt -y update" "apt -y update" "yum -y update" "yum -y update")
+PACKAGE_INSTALL=("apt -y install" "apt -y install" "yum -y install" "yum -y install" "apk add -f")
+PACKAGE_REMOVE=("apt -y remove" "apt -y remove" "yum -y remove" "yum -y remove")
 
+[[ $EUID -ne 0 ]] && yellow "请在root用户下运行脚本" && exit 1
+
+CMD=("$(grep -i pretty_name /etc/os-release 2>/dev/null | cut -d \" -f2)" "$(hostnamectl 2>/dev/null | grep -i system | cut -d : -f2)" "$(lsb_release -sd 2>/dev/null)" "$(grep -i description /etc/lsb-release 2>/dev/null | cut -d \" -f2)" "$(grep . /etc/redhat-release 2>/dev/null)" "$(grep . /etc/issue 2>/dev/null | cut -d \\ -f1 | sed '/^[ ]*$/d')")
+
+for i in "${CMD[@]}"; do
+    SYS="$i" && [[ -n $SYS ]] && break
+done
+
+for ((int = 0; int < ${#REGEX[@]}; int++)); do
+    [[ $(echo "$SYS" | tr '[:upper:]' '[:lower:]') =~ ${REGEX[int]} ]] && SYSTEM="${RELEASE[int]}" && [[ -n $SYSTEM ]] && break
+done
+
+[[ -z $SYSTEM ]] && red "不支持当前VPS的操作系统，请使用主流的操作系统" && exit 1
+if [[ -z $(type -P curl) ]]; then
+    if [[ ! $SYSTEM == "CentOS" ]]; then
+        ${PACKAGE_UPDATE[int]}
+    fi
+    ${PACKAGE_INSTALL[int]} curl
+fi
 # 检查是否为root用户
 [[ $EUID -ne 0 ]] && echo -e "${RED}错误：${PLAIN} 必须使用root用户运行此脚本！\n" && exit 1
 
@@ -67,10 +91,25 @@ generate_port() {
 generate_ws_path() {
     echo "/$(tr -dc 'a-zA-Z0-9' < /dev/urandom | head -c 10)"
 }
+checkStatus() {
+    [[ -z $(cloudflared -help 2>/dev/null) ]] && cloudflaredStatus="未安装"
+    [[ -n $(cloudflared -help 2>/dev/null) ]] && cloudflaredStatus="已安装"
+    [[ -f /root/.cloudflared/cert.pem ]] && loginStatus="已登录"
+    [[ ! -f /root/.cloudflared/cert.pem ]] && loginStatus="未登录"
+}
 
-mkdir -p /root/catmi
+archAffix() {
+    case "$(uname -m)" in
+        i686 | i386) echo '386' ;;
+        x86_64 | amd64) echo 'amd64' ;;
+        armv5tel | arm6l | armv7 | armv7l) echo 'arm' ;;
+        armv8 | arm64 | aarch64) echo 'aarch64' ;;
+        *) red "不支持的CPU架构！" && exit 1 ;;
+    esac
+}
 installCloudFlared() {
-  
+    # 检查是否已安装 cloudflared
+    [[ $cloudflaredStatus == "已安装" ]] && red "检测到已安装并登录CloudFlare Argo Tunnel，无需重复安装！！" && exit 1
     
     # 获取最新版本
     last_version=$(curl -Ls "https://api.github.com/repos/cloudflare/cloudflared/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
@@ -102,7 +141,7 @@ installCloudFlared() {
 
 
 loginCloudFlared(){
-   
+    [[ $loginStatus == "已登录" ]] && red "检测到已登录CloudFlare Argo Tunnel，无需重复登录！！" && exit 1
     cloudflared tunnel login
     checkStatus
     if [[ $cloudflaredStatus == "未登录" ]]; then
@@ -145,7 +184,8 @@ EOF
 }
 
 runTunnel() {
-   
+    [[ $cloudflaredStatus == "未安装" ]] && red "检测到未安装CloudFlare Argo Tunnel客户端，无法执行操作！！！" && exit 1
+    [[ $loginStatus == "未登录" ]] && red "请登录CloudFlare Argo Tunnel客户端后再执行操作！！！" && exit 1
     [[ -z $(type -P screen) ]] && ${PACKAGE_UPDATE[int]} && ${PACKAGE_INSTALL[int]} screen
     
    
@@ -154,7 +194,8 @@ runTunnel() {
     back2menu
 }
 argoCert() {
-    
+    [[ $cloudflaredStatus == "未安装" ]] && red "检测到未安装CloudFlare Argo Tunnel客户端，无法执行操作！！！" && exit 1
+    [[ $loginStatus == "未登录" ]] && red "请登录CloudFlare Argo Tunnel客户端后再执行操作！！！" && exit 1
     sed -n "1, 5p" /root/.cloudflared/cert.pem >>/root/private.key
     sed -n "6, 24p" /root/.cloudflared/cert.pem >>/root/cert.crt
     green "CloudFlare Argo Tunnel证书提取成功！"
@@ -579,9 +620,11 @@ echo "${share_link}" > /root/catmi/xray.txt
 sudo systemctl status xrayls
 }
 menu() {
+   mkdir -p /root/catmi
     echo "执行 input_variables"
     input_variables
-  
+    checkStatus
+    archAffix
     echo "执行 installCloudFlared"
     installCloudFlared
     echo "执行 loginCloudFlared"
