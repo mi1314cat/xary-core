2aaebc2579dd54dda52cfb45d82b31443e8ea699
mi1314cat <135564322+mi1314cat@users.noreply.github.com>
2026-04-27 12:07:03 +0800

    Update xray-panel.sh

--- xray-panel.sh
+++ xray-panel.sh
@@ -47,7 +47,7 @@ xrayls 服务状态: ${xrayls_server_status_text}
         4) systemctl restart xrayls.service ;;
         5) show_xray_configs ;;
         6) XRevise ;;  # 示例配置修改
-        7) systemctl status xrayls.service -l ;;
+        7) systemctl status xrayls --no-pager ;;
         8) add_node_menu ;;
 
 
